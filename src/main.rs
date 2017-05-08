e40e00861f222b0564149e08486a21d8dcc8bf2c
Ross Harrison <rtharrison86@gmail.com>
2017-05-07 23:42:21 -0400

    insert into mongo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-//#[macro_use(bson, doc)]
-//extern crate bson;
-//extern crate mongodb;
+#[macro_use(doc, bson)]
+extern crate bson;
+#[macro_use]
+extern crate serde_derive;
+
+extern crate mongodb;
 #[macro_use(load_yaml)]
 extern crate clap;
 
 mod note;
 
-//use bson::Bson;
-//use mongodb::{ Client, ThreadedClient};
-//use mongodb::db::ThreadedDatabase;
+use mongodb::{ Client, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
 use clap::{App};
 use note::Note;
 
+static db_path: &'static str = "~/.jot.db";
+
+// save a new note
+fn saveNote (note: Note) {
+    let client = Client::connect("localhost", 27017)
+        .expect("Failed to initialize client");
+    let collection = client.db("taker").collection("notes");
+    collection.insert_one(note.to_doc(), None).expect("couldn't insert");
+    println!("note saved: {:?}", note);
+}
 
 fn main () {
     let yaml = load_yaml!("cli.yml");
@@ -23,44 +35,22 @@ fn main () {
         let tag_str = matches.value_of("tags").unwrap_or("no tags");
         println!("tags: {}", tag_str);
 
-        if let Some(queryString) = matches.value_of("QUERY") {
-            println!("query by: {}", queryString);
+        if let Some(query_string) = matches.value_of("QUERY") {
+            println!("query by: {}", query_string);
         }
 
     } else  {
         println!("adding a note");
 
-        if let Some(fileInput) = matches.value_of("file") {
-            println!("File input: {}", fileInput);
+        if let Some(file_input) = matches.value_of("file") {
+            println!("File input: {}", file_input);
         }
 
-        if let Some(noteContentStr) = matches.value_of("NOTE_CONTENT") {
-            let note = Note::new(noteContentStr);
-            println!("noteContent: {:?}", note);
+        if let Some(note_content) = matches.value_of("NOTE_CONTENT") {
+            saveNote(Note::new(note_content));
         }
     }
 
     println!("matches: {:?}", matches);
 }
 
-/*fn main() {
-    //let client = Client::with_uri("mongodb://rharriso:Q55YMIZEj08a@ds129641.mlab.com:29641/")
-    //    .expect("Failed to initialize");
-    let client = Client::connect("localhost", 27017)
-        .expect("Failed to initialize client");
-
-    let collection = client.db("taker").collection("notes");
-
-    collection.insert_one(doc!{ "title" => "My guy is my guy ok guy?"}, None)
-        .ok().expect("Failed to insert document.");
-
-    let mut cursor = collection.find(None, None).unwrap();
-    for result in cursor {
-        if let Ok(item) = result {
-            if let Some(&Bson::String(ref title)) = item.get("title") {
-                println!("title: {}", title);
-            }
-        }
-    }
-}
-*/
--- src/note.rs
+++ src/note.rs
@@ -1,14 +1,17 @@
-extern crate chrono;
 extern crate uuid;
+extern crate chrono;
+extern crate bson;
 
 use self::chrono::prelude::*;
 use self::uuid::Uuid;
+use self::bson::Document;
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Note {
-    content: String,
-    created_at: DateTime<UTC>,
-    uuid: String,
+    pub content: String,
+    //pub created_at: DateTime<UTC>,
+    #[serde(rename = "_id")]
+    pub uuid: String,
 }
 
 impl Note {
@@ -16,8 +19,16 @@ impl Note {
     pub fn new(content: &str) -> Note {
         return Note{
             content: content.to_string(),
-            created_at: UTC::now(),
+//            created_at: UTC::now(),
             uuid: Uuid::new_v4().to_string()
         }
+    }
+
+    pub fn to_doc(&self) -> bson::Document {
+        let mut doc = Document::new();
+        doc.insert("content", self.content.to_string());
+        doc.insert("uuid", self.uuid.to_string());
+ //     doc.insert("created_at", self.created_at);
+        return doc;
     }
 }
